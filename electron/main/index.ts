02c9e8153fd346ea18dd6ea17a47444bb2e0bed3
Islam Ahmed <softeng.islam@gmail.com>
2023-08-25 05:04:17 +0300

    Database Update

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -250,14 +250,11 @@ function createDbConnection() {
 
 // createDbConnection();
 function createTable(db) {
-	db.exec(`
-  CREATE TABLE sharks
-  (
+	db.exec(`CREATE TABLE sharks(
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
     name   VARCHAR(50) NOT NULL,
     color  VARCHAR(50) NOT NULL,
-    weight INTEGER NOT NULL
-  );
+    weight INTEGER NOT NULL);
 `);
 }
 // createTable(createDbConnection());

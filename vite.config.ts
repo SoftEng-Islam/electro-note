06a7fd9f52ac63f05435759064caf5bba92e89f9
Islam Ahmed <softeng.islam@gmail.com>
2023-08-26 07:21:58 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import electron from "vite-plugin-electron";
 import renderer from "vite-plugin-electron-renderer";
-import pkg from "./package.json";
+import pkg from './package.json';
+
 import vuePugPlugin from "vue-pug-plugin";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
 	rmSync("dist-electron", { recursive: true, force: true });
 
@@ -19,7 +19,6 @@ export default defineConfig(({ command }) => {
 			vue({
 				template: {
 					preprocessOptions: {
-						// 'preprocessOptions' is passed through to the pug compiler
 						plugins: [vuePugPlugin],
 					},
 				},

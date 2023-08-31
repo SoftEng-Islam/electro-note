5b454e78cf33dd8165d88096e7c67f9554ba3e69
Islam Ahmed <softeng.islam@gmail.com>
2023-08-31 22:52:47 +0300

    Update Tiptap

--- src/components/Tiptap.vue
+++ src/components/Tiptap.vue
@@ -99,9 +99,7 @@ let hoverdColor = ref("white");
 const NameOfColors = ["gray","yellow","green","cyan","blue","purple","fuchsia","pink"];
 const RangeOfColors = [100,200,300,400,500,600,700,800,900,950];
 function addColor(color: string) {
-	// console.log(color);
-	// return editor.value?.chain().focus().setColor('#fff').run();
-	return editor.value?.commands.toggleWrap('heading', { class: 'dsf' });
+	return editor.value?.chain().focus().setColor('#fff').run();
 }
 
 </script>

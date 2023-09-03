87c9635ef35e79817329dc4bf0e9df1b7ba0084d
Islam Ahmed <softeng.islam@gmail.com>
2023-09-03 06:01:49 +0300

    Update Header.vue

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -66,10 +66,8 @@ header(class="border-[var(--dark200)] border-b w-full flex items-center p-2 sele
 	div(class="toggleMode" style="-webkit-app-region: no-drag")
 		input(type="checkbox" id="darkmode-toggle")
 		label(for="darkmode-toggle")
-			<svg version="1.1" class="sun" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 496 496" style="enable-background:new 0 0 496 496;" xml:space="preserve"> <rect x="152.994" y="58.921" transform="matrix(0.3827 0.9239 -0.9239 0.3827 168.6176 -118.5145)" width="40.001" height="16"/> <rect x="46.9" y="164.979" transform="matrix(0.9239 0.3827 -0.3827 0.9239 71.29 -12.4346)" width="40.001" height="16"/> <rect x="46.947" y="315.048" transform="matrix(0.9239 -0.3827 0.3827 0.9239 -118.531 50.2116)" width="40.001" height="16"/> <rect x="164.966" y="409.112" transform="matrix(-0.9238 -0.3828 0.3828 -0.9238 168.4872 891.7491)" width="16" height="39.999"/> <rect x="303.031" y="421.036" transform="matrix(-0.3827 -0.9239 0.9239 -0.3827 50.2758 891.6655)" width="40.001" height="16"/> <rect x="409.088" y="315.018" transform="matrix(-0.9239 -0.3827 0.3827 -0.9239 701.898 785.6559)" width="40.001" height="16"/> <rect x="409.054" y="165.011" transform="matrix(-0.9239 0.3827 -0.3827 -0.9239 891.6585 168.6574)" width="40.001" height="16"/> <rect x="315.001" y="46.895" transform="matrix(0.9238 0.3828 -0.3828 0.9238 50.212 -118.5529)" width="16" height="39.999"/> <path d="M248,88c-88.224,0-160,71.776-160,160s71.776,160,160,160s160-71.776,160-160S336.224,88,248,88z M248,392 c-79.4,0-144-64.6-144-144s64.6-144,144-144s144,64.6,144,144S327.4,392,248,392z"/> <rect x="240" width="16" height="72"/> <rect x="62.097" y="90.096" transform="matrix(0.7071 0.7071 -0.7071 0.7071 98.0963 -40.6334)" width="71.999" height="16"/> <rect y="240" width="72" height="16"/> <rect x="90.091" y="361.915" transform="matrix(-0.7071 -0.7071 0.7071 -0.7071 -113.9157 748.643)" width="16" height="71.999"/> <rect x="240" y="424" width="16" height="72"/> <rect x="361.881" y="389.915" transform="matrix(-0.7071 -0.7071 0.7071 -0.7071 397.8562 960.6281)" width="71.999" height="16"/> <rect x="424" y="240" width="72" height="16"/> <rect x="389.911" y="62.091" transform="matrix(0.7071 0.7071 -0.7071 0.7071 185.9067 -252.6357)" width="16" height="71.999"/> </svg>
-			<svg version="1.1" class="moon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 49.739 49.739" style="enable-background:new 0 0 49.739 49.739;" xml:space="preserve"> <path d="M25.068,48.889c-9.173,0-18.017-5.06-22.396-13.804C-3.373,23.008,1.164,8.467,13.003,1.979l2.061-1.129l-0.615,2.268 c-1.479,5.459-0.899,11.25,1.633,16.306c2.75,5.493,7.476,9.587,13.305,11.526c5.831,1.939,12.065,1.492,17.559-1.258v0 c0.25-0.125,0.492-0.258,0.734-0.391l2.061-1.13l-0.585,2.252c-1.863,6.873-6.577,12.639-12.933,15.822 C32.639,48.039,28.825,48.888,25.068,48.889z M12.002,4.936c-9.413,6.428-12.756,18.837-7.54,29.253 c5.678,11.34,19.522,15.945,30.864,10.268c5.154-2.582,9.136-7.012,11.181-12.357c-5.632,2.427-11.882,2.702-17.752,0.748 c-6.337-2.108-11.473-6.557-14.463-12.528C11.899,15.541,11.11,10.16,12.002,4.936z"/></svg>
-
-
+			<svg viewBox="0 0 24 24" class="sun" xmlns="http://www.w3.org/2000/svg"><path d="M12 18C8.68629 18 6 15.3137 6 12C6 8.68629 8.68629 6 12 6C15.3137 6 18 8.68629 18 12C18 15.3137 15.3137 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16ZM11 1H13V4H11V1ZM11 20H13V23H11V20ZM3.51472 4.92893L4.92893 3.51472L7.05025 5.63604L5.63604 7.05025L3.51472 4.92893ZM16.9497 18.364L18.364 16.9497L20.4853 19.0711L19.0711 20.4853L16.9497 18.364ZM19.0711 3.51472L20.4853 4.92893L18.364 7.05025L16.9497 5.63604L19.0711 3.51472ZM5.63604 16.9497L7.05025 18.364L4.92893 20.4853L3.51472 19.0711L5.63604 16.9497ZM23 11V13H20V11H23ZM4 11V13H1V11H4Z"/></svg>
+			<svg viewBox="0 0 24 24" class="moon" xmlns="http://www.w3.org/2000/svg"><path d="M10 7C10 10.866 13.134 14 17 14C18.9584 14 20.729 13.1957 21.9995 11.8995C22 11.933 22 11.9665 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C12.0335 2 12.067 2 12.1005 2.00049C10.8043 3.27098 10 5.04157 10 7ZM4 12C4 16.4183 7.58172 20 12 20C15.0583 20 17.7158 18.2839 19.062 15.7621C18.3945 15.9187 17.7035 16 17 16C12.0294 16 8 11.9706 8 7C8 6.29648 8.08133 5.60547 8.2379 4.938C5.71611 6.28423 4 8.9417 4 12Z"/></svg>
 
 	//- User Section
 	div(class="flex items-center justify-center" style="-webkit-app-region: no-drag")
@@ -90,7 +88,7 @@ header(class="border-[var(--dark200)] border-b w-full flex items-center p-2 sele
 </template>
 <style scoped lang="scss">
 .toggleMode {
-	@apply h-8 w-20 mx-3 flex items-center justify-center overflow-hidden;
+	@apply h-8 w-20 mx-3 flex flex-row items-center justify-center bg-[var(--dark100)] rounded-full;
 	input {
 		@apply w-0 h-0;
 		visibility: hidden;
@@ -98,7 +96,7 @@ header(class="border-[var(--dark200)] border-b w-full flex items-center p-2 sele
 			@apply bg-[#242424];
 		}
 		&:checked + label:after {
-			@apply left-20;
+			@apply left-full;
 			transform: translateX(-100%);
 			background: linear-gradient(180deg,#777,#3a3a3a);
 		}
@@ -111,8 +109,7 @@ header(class="border-[var(--dark200)] border-b w-full flex items-center p-2 sele
 	}
 
 	label {
-		@apply w-full h-8 relative flex items-center bg-[#ebebeb] rounded-full cursor-pointer duration-300;
-		box-shadow: inset 0px 5px 15px rgba(0,0,0,0.4), inset 0px -5px 15px rgba(255,255,255,0.4);
+		@apply w-full h-full relative flex flex-row items-center justify-center bg-[#ebebeb] rounded-full cursor-pointer duration-300;
 		&:after {
 			content: "";
 			@apply w-8 h-8 absolute left-0 rounded-full duration-300;
@@ -120,7 +117,7 @@ header(class="border-[var(--dark200)] border-b w-full flex items-center p-2 sele
 			box-shadow: 0px 5px 10px rgba(0,0,0,0.2);
 		}
 		& svg {
-			@apply absolute w-7 top-0 z-10;
+			@apply absolute w-7 z-10;
 		}
 		& svg.sun {
 			@apply left-0 fill-white duration-300;

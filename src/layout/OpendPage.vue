224b1e1ec513cc3e39b40c17faa693a5237e3206
Islam Ahmed <softeng.islam@gmail.com>
2023-09-15 06:26:46 +0300

    Update OpendPage.vue

--- src/layout/OpendPage.vue
+++ src/layout/OpendPage.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+
+// Tiptap Extension
 import Table from "@tiptap/extension-table";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
@@ -8,12 +10,14 @@ import toggleUnderline from "@tiptap/extension-underline";
 import Subscript from "@tiptap/extension-subscript";
 import Superscript from "@tiptap/extension-superscript";
 import TextStyle from "@tiptap/extension-text-style";
+import Paragraph from '@tiptap/extension-paragraph'
 import { Color } from "@tiptap/extension-color";
 import { Highlight } from "@tiptap/extension-highlight";
-import Paragraph from '@tiptap/extension-paragraph'
+
+// Import Tiptap
 import { useEditor, EditorContent, Extension } from "@tiptap/vue-3";
-import StarterKit from "@tiptap/starter-kit";
 import { ref, watch } from "vue";
+import StarterKit from "@tiptap/starter-kit";
 
 
 const content = ref(`<h2> Hi there, </h2> <p> this is a basic <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists: </p> <ul> <li> That’s a bullet list with one … </li> <li> … or two list items. </li> </ul> <p> Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block: </p> <pre><code class="language-css">body { display: none; }</code></pre> <p> I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too. </p> <blockquote> Wow, that’s amazing. Good work, boy! 👏 <br /> — Mom </blockquote>`);

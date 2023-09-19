7a3a4fb93a5f7f74223cdb8b259def5722bc0f60
Islam Ahmed <softeng.islam@gmail.com>
2023-09-19 21:32:01 +0300

    Update LoginApp.vue

--- src/components/LoginApp.vue
+++ src/components/LoginApp.vue
@@ -10,7 +10,7 @@
 </script>
 <template lang="pug">
 div(class="z-40 fixed w-screen h-full left-0 top-0 backdrop-blur")
-div(class="z-50 flex flex-col items-center justify-center fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-72 p-3 rounded-3xl dark:bg-[var(--dark400)] shadow-lg")
+div(class="z-50 flex flex-col items-center justify-center fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-72 p-3 rounded-3xl dark:bg-[var(--dark400)] border-1 shadow-lg")
 	//- Avatar
 	div(class="w-full flex flex-col items-center justify-center")
 		div(class="")

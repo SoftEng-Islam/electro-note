028886735263cce81dad9935585349da14b74045
Islam Ahmed <softeng.islam@gmail.com>
2023-09-23 22:35:46 +0300

    Update index.ts

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -172,7 +172,7 @@ async function createWindow() {
 			preload,
 			// Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
 			// Consider using contextBridge.exposeInMainWorld
-      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+			// Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
 			nodeIntegration: true,
 			contextIsolation: false,
 		},

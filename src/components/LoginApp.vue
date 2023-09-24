0cb8d157ffd7ecbf420f981a4d9628b6fa9b2d11
Islam Ahmed <softeng.islam@gmail.com>
2023-09-25 00:33:36 +0300

    Update LoginApp.vue

--- src/components/LoginApp.vue
+++ src/components/LoginApp.vue
@@ -13,7 +13,8 @@ export default {
 			PasswordValid: false,
 			validUser: '',
 			eyeOFF: true,
-			eyeON: false
+			eyeON: false,
+			PassType: 'password'
 		}
 	},
 	setup() {
@@ -46,6 +47,11 @@ export default {
 		passWordValidator() {
 			this.passWord.length > 3 ? this.PasswordValid = true : this.PasswordValid = false;
 		},
+		showPass() {
+			this.eyeOFF = !this.eyeOFF;
+			this.eyeON = !this.eyeON;
+			this.PassType === 'password' ? this.PassType = 'text' : this.PassType = 'password'
+		}
 	}
 }
 </script>
@@ -63,17 +69,17 @@ div(v-show="true" class="z-50 flex flex-col items-center justify-center fixed le
 	div(class="flex flex-col items-center justify-center gap-y-3 py-4")
 		//- Full Name
 		div(class="flex items-center gap-x-1 ")
-			span(v-show="fullName !== ''" class="absolute right-10 w-3 h-3 rounded-full border border-gray-800" :class="FullNameValid ? 'bg-green-500':'bg-red-500'")
+			span(v-show="fullName !== ''" class="absolute right-6 w-3 h-3 rounded-full border border-gray-800" :class="FullNameValid ? 'bg-green-500':'bg-red-500'")
 			input(class="outline-1 focus:outline-blue-500 rounded-xl" placeholder="Full Name" v-on:input="fullNameValidator" v-model="fullName" type="text")
 		//- Username
 		div(class="flex items-center gap-x-1 ")
-			span(v-show="userName !== ''" class="absolute right-10 w-3 h-3 rounded-full border border-gray-800" :class="UsernameValid ? 'bg-green-500':'bg-red-500'")
+			span(v-show="userName !== ''" class="absolute right-6 w-3 h-3 rounded-full border border-gray-800" :class="UsernameValid ? 'bg-green-500':'bg-red-500'")
 			input(class="outline-1 focus:outline-blue-500 rounded-xl" placeholder="Username" v-on:input="userNameValidator" v-model="userName" type="text")
 		//- Password
 		div(class="flex items-center gap-x-1 ")
-			span(v-show="passWord !== ''" class="absolute right-10 w-3 h-3 rounded-full border border-gray-800" :class="PasswordValid ? 'bg-green-500':'bg-red-500'")
-			input(class="outline-1 focus:outline-blue-500 rounded-xl" placeholder="Password" v-on:input="passWordValidator" v-model="passWord" type="password")
-			button(type="button" class="w-5 h-full absolute right-8 cursor-pointer" @click="")
+			span(v-show="passWord !== ''" class="absolute right-6 w-3 h-3 rounded-full border border-gray-800" :class="PasswordValid ? 'bg-green-500':'bg-red-500'")
+			input(class="outline-1 focus:outline-blue-500 rounded-xl" placeholder="Password" v-on:input="passWordValidator" v-model="passWord" :type="PassType")
+			button(type="button" class="w-5 h-full absolute right-12 cursor-pointer" @click="showPass")
 				<svg v-show="eyeON"  class="w-4 mx-auto" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M1.18164 12C2.12215 6.87976 6.60812 3 12.0003 3C17.3924 3 21.8784 6.87976 22.8189 12C21.8784 17.1202 17.3924 21 12.0003 21C6.60812 21 2.12215 17.1202 1.18164 12ZM12.0003 17C14.7617 17 17.0003 14.7614 17.0003 12C17.0003 9.23858 14.7617 7 12.0003 7C9.23884 7 7.00026 9.23858 7.00026 12C7.00026 14.7614 9.23884 17 12.0003 17ZM12.0003 15C10.3434 15 9.00026 13.6569 9.00026 12C9.00026 10.3431 10.3434 9 12.0003 9C13.6571 9 15.0003 10.3431 15.0003 12C15.0003 13.6569 13.6571 15 12.0003 15Z"/></svg>
 				<svg v-show="eyeOFF" class="w-4 mx-auto" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4.52047 5.93457L1.39366 2.80777L2.80788 1.39355L22.6069 21.1925L21.1927 22.6068L17.8827 19.2968C16.1814 20.3755 14.1638 21.0002 12.0003 21.0002C6.60812 21.0002 2.12215 17.1204 1.18164 12.0002C1.61832 9.62282 2.81932 7.5129 4.52047 5.93457ZM14.7577 16.1718L13.2937 14.7078C12.902 14.8952 12.4634 15.0002 12.0003 15.0002C10.3434 15.0002 9.00026 13.657 9.00026 12.0002C9.00026 11.537 9.10522 11.0984 9.29263 10.7067L7.82866 9.24277C7.30514 10.0332 7.00026 10.9811 7.00026 12.0002C7.00026 14.7616 9.23884 17.0002 12.0003 17.0002C13.0193 17.0002 13.9672 16.6953 14.7577 16.1718ZM7.97446 3.76015C9.22127 3.26959 10.5793 3.00016 12.0003 3.00016C17.3924 3.00016 21.8784 6.87992 22.8189 12.0002C22.5067 13.6998 21.8038 15.2628 20.8068 16.5925L16.947 12.7327C16.9821 12.4936 17.0003 12.249 17.0003 12.0002C17.0003 9.23873 14.7617 7.00016 12.0003 7.00016C11.7514 7.00016 11.5068 7.01833 11.2677 7.05343L7.97446 3.76015Z"/></svg>
 	//- create account or Login

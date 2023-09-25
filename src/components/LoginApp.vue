25c84181207f2e576673b0a4e98ab2895efb8c90
Islam Ahmed <softeng.islam@gmail.com>
2023-09-26 00:20:31 +0300

    update

--- src/components/LoginApp.vue
+++ src/components/LoginApp.vue
@@ -36,8 +36,8 @@ export default {
 		}
 	},
 	watch: {
-		validUser(vLU){
-			if (vLU === true) {
+		validUser(newValue, oldValue){
+			if (newValue === true) {
 				console.log('true')
 			} else {
 				console.log('false')
